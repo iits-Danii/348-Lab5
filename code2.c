660c3d9a2a88e37024a8c12d4c0b1fa7397edc70
Daniel Harris <danharri@ku.edu>
2024-10-13 23:18:09 -0500

    Finished first iterations of add, multiply, and their respective checks

--- code2.c
+++ code2.c
@@ -10,6 +10,8 @@ void add(int m1[SIZE][SIZE],int m2[SIZE][SIZE]);
 void mult(int m1[SIZE][SIZE],int m2[SIZE][SIZE]);
 void transpose(int m1[SIZE][SIZE],int m2[SIZE][SIZE]);
 void print(int m1[SIZE][SIZE],int m2[SIZE][SIZE]);
+int dim_check_a(int r1, int c1, int r2, int c2);
+int dim_check_m(int r1, int c1, int r2, int c2);
 
 int main() {
     int option = 0;
@@ -32,24 +34,59 @@ int main() {
     scanf("%d", &option);
 
     if (option == 1) {
-        add(m1, m2);
+        if (dim_check_a(SIZE, SIZE, SIZE, SIZE) == 1) {
+            add(m1, m2);
+        } else {
+            return 0;
+        }
     } else if (option == 2) {
-        mult(m1, m2);
+        if (dim_check_m(SIZE, SIZE, SIZE, SIZE) == 1) {
+            add(m1, m2);
+        } else {
+            return 0;
+        }
     } else if (option ==3) {
         transpose(m1, m2);
     } else if (option == 4) {
         print(m1, m2);
+    } else {
+        printf("Invalid input.");
     }
 }
 
 void add(int m1[SIZE][SIZE],int m2[SIZE][SIZE]) {
     int m3[SIZE][SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; i < SIZE; i++) {
+            m3[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+    printf("The matrix after addition:\n");
+    for (int i = 0; i < SIZE; i++) {
+            printf("\n");
+            for (int j = 0; j < SIZE; j++) {
+                printf("%d\t", m3[i][j]);
+            }
+        }
 
 }
 
 void mult(int m1[SIZE][SIZE],int m2[SIZE][SIZE]) {
     int m3[SIZE][SIZE];
-
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            for (int k = 0; k < SIZE; k++) {
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    printf("The matrix after multiplication:\n");
+    for (int i = 0; i < SIZE; i++) {
+            printf("\n");
+            for (int j = 0; j < SIZE; j++) {
+                printf("%d\t", m3[i][j]);
+            }
+        }
 }
 
 void transpose(int m1[SIZE][SIZE],int m2[SIZE][SIZE]) {
@@ -94,4 +131,22 @@ void print(int m1[SIZE][SIZE],int m2[SIZE][SIZE]) {
         }
     }
 
+}
+
+int dim_check_a(int r1, int c1, int r2, int c2) {
+    if ((r1 == r2) && (c1 == c2)) {
+        return 1;
+    } else {
+        printf("Incompatible dimensions.");
+        return 0;
+    }
+}
+
+int dim_check_m(int r1, int c1, int r2, int c2) {
+    if ((c1 == r2)) {
+        return 1;
+    } else {
+        printf("Incompatible dimensions.");
+        return 0;
+    }
 }
